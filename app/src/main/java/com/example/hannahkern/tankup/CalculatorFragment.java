42bf41a0ef3d37d98796aedb0fff11c3b600f75d
Paulina0710 <paulina.wittich@gmx.de>
2018-03-01 15:46:00 +0100

    test

--- app/src/main/java/com/example/hannahkern/tankup/CalculatorFragment.java
+++ app/src/main/java/com/example/hannahkern/tankup/CalculatorFragment.java
@@ -40,8 +40,8 @@ public class CalculatorFragment extends Fragment{
 
         View v = inflater.inflate(R.layout.fragment_calculator, container, false);
 
-        /*mGas = (EditText) v.findViewById(R.id.enter_price);
-            mGas.setText( mCalculator.getGas());
+            mGas = (EditText) v.findViewById(R.id.enter_price);
+            /*mGas.setText( mCalculator.getGas());
             mGas.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -50,18 +50,18 @@ public class CalculatorFragment extends Fragment{
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    mCalculator.setGas(s.toString());
+                    //mCalculator.setGas(s.toString());
                 }
 
                 @Override
                 public void afterTextChanged(Editable s) {
                     // This one too
                 }
-            });
+            });*/
 
 
             mKm = (EditText) v.findViewById(R.id.enter_distance);
-            mKm.setText(mCalculator.getKm());
+            /*mKm.setText(mCalculator.getKm());
             mKm.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -77,39 +77,46 @@ public class CalculatorFragment extends Fragment{
                 public void afterTextChanged(Editable s) {
                     //This one too
                 }
-            });
+            });*/
 
             mCalculateButton = (Button) v.findViewById(R.id.calculateButton);
             mCalculateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 mResultText = (TextView) v.findViewById(R.id.result);
 
-                // Gets the two EditText controls' Editable values
-                Editable editableGas = mGas.getText(),
-                        editableKm = mKm.getText();
+                calculate();
 
-                // Initializes the double values and result
-                double value1 = 0.0,
-                        value2 = 0.0,
-                        result;
-
-                // If the Editable values are not null, obtains their double values by parsing
-                if (editableGas != null)
-                    value1 = Double.parseDouble(editableGas.toString());
-
-                if (editableKm != null)
-                    value2 = Double.parseDouble(editableKm.toString());
-
-                // Calculates the result
-                result = value1 * value2;
-
-                // Displays the calculated result
-                mResultText.setText(String.valueOf(result));
                 }
-            });*/
+            });
 
             return v;
 
+        }
+
+        public void calculate(double value1, double value2){
+            // Gets the two EditText controls' Editable values
+            Editable editableGas = mGas.getText(),
+                    editableKm = mKm.getText();
+
+            // Initializes the double values and result
+            double value1 = 0.0,
+                    value2 = 0.0,
+                    result;
+
+            // If the Editable values are not null, obtains their double values by parsing
+            if (editableGas != null)
+                value1 = Double.parseDouble(editableGas.toString());
+
+            if (editableKm != null)
+                value2 = Double.parseDouble(editableKm.toString());
+
+
+            // Calculates the result
+            result = value1 * value2;
+            String ergebnis = Double.toString(result);
+
+            // Displays the calculated result
+            return ergebnis;
         }
 
 }
