6292c7c609b4d82f0fe58256d689d36a7e6d9e5d
Hankern1996 <hannah_kern2007@web.de>
2018-03-01 13:06:16 +0100

    Mit Test fragment und Roadtrip klasse

--- app/src/main/java/com/example/hannahkern/tankup/MapFragment.java
+++ app/src/main/java/com/example/hannahkern/tankup/MapFragment.java
@@ -1,11 +1,18 @@
 package com.example.hannahkern.tankup;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by hannahkern on 28.02.18.
@@ -13,16 +20,57 @@ import android.view.ViewGroup;
 
 public class MapFragment extends Fragment{
 
+    private Roadtrip mRoadtrip;
+    private EditText mGas;
+    private Button mNextButton;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
-        return  v;}
+
+            View v = inflater.inflate(R.layout.fragment_test, container, false);
+            mGas = (EditText) v.findViewById(R.id.enter_price);
+            mGas.setText( mRoadtrip.getGas());
+            mGas.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mRoadtrip.setGas(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This one too
+            }
+        });
+
+
+        mNextButton = (Button) v.findViewById(R.id.calculate);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MapFragment.class);
+                startActivity(i);
+            }
+        });
+
+
+        Log.i("Hier",mGas.toString());
+
+
+            return v;
+
+
+        }
 
 }
---
+++ app/src/main/java/com/example/hannahkern/tankup/Roadtrip.java
@@ -0,0 +1,40 @@
+package com.example.hannahkern.tankup;
+
+/**
+ * Created by hannahkern on 28.02.18.
+ */
+
+public class Roadtrip {
+
+    private String gas;
+
+    public String getGas() {
+        return gas;
+    }
+
+    public void setGas(String gas) {
+        this.gas = gas;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public double getErgebnis() {
+        return ergebnis;
+    }
+
+    public void setErgebnis(double ergebnis) {
+        this.ergebnis = ergebnis;
+    }
+
+    private double km;
+    private double ergebnis;
+
+
+
+}
