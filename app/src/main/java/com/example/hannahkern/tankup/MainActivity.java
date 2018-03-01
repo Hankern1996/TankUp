ead4d435d2175896208c8e02903ac2163a7d5bf3
Hankern1996 <hannah_kern2007@web.de>
2018-03-01 13:33:08 +0100

    new Main Activity (reboot)

--- app/src/main/java/com/example/hannahkern/tankup/MainActivity.java
+++ app/src/main/java/com/example/hannahkern/tankup/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
     }
 
     @Override
@@ -65,7 +64,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    /*public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -76,49 +75,27 @@ public class MainActivity extends AppCompatActivity
             return true;
         }
 
-        return super.onOptionsItemSelected(item);*/
-
+        return super.onOptionsItemSelected(item);
+    }
 
     @SuppressWarnings("StatementWithEmptyBody")
-    //@Override
+    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        if (id == R.id.map) {
+            // Handle the camera action
+        } else if (id == R.id.calculate) {
 
-        if (id == R.id.nav_map) {
+        } else if (id == R.id.nav_share) {
 
+        } else if (id == R.id.nav_profile) {
 
-            setContentView(R.layout.fragment_test);
-
-            FragmentManager fm = getSupportFragmentManager();
-            Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-            if (fragment == null) {
-                fragment = new MapFragment();
-                fm.beginTransaction()
-                        .add(R.id.fragment_container, fragment)
-                        .commit();
-
-
-             /*fragment = new MapFragment();
-             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-             ft.replace(R.id.fragment_container, fragment);
-             ft.commit();*/
-
-             /*fragment = new MapFragment();
-             fm.beginTransaction()
-                     .add(R.id.fragment_container, fragment)
-                     .commit();*/
-
-            } else if (id == R.id.nav_share) {
-
-            } else if (id == R.id.nav_profile) {
-
-            }
-
+        } else if (id == R.id.nav_share) {
 
         }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
