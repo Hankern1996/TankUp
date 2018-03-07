0fddad768274db8f06d61540e5fab007440a6cba
Hankern1996 <hannah_kern2007@web.de>
2018-03-07 10:58:58 +0100

    Gradle Build and new try with maps, erste Schritte

--- app/src/main/java/com/example/hannahkern/tankup/MapsActivity.java
+++ app/src/main/java/com/example/hannahkern/tankup/MapsActivity.java
@@ -209,6 +209,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
