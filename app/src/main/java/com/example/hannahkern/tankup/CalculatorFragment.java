4300ace630f3df3261e3cd3354624343364ea97a
Hankern1996 <hannah_kern2007@web.de>
2018-03-12 16:22:18 +0100

    Facebook SDK

--- app/src/main/java/com/example/hannahkern/tankup/CalculatorFragment.java
+++ app/src/main/java/com/example/hannahkern/tankup/CalculatorFragment.java
@@ -18,6 +18,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
 
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
